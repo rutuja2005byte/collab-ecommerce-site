aa63cd819a4d8bf5a16437b90fa15d88029e12a7
Rutuja darade <rutujadarade2005>
2026-03-16 19:02:18 +0530

    complete project

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName:'Flodable Mini Drone',
+        productNumber: '85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Lavender KF102 Drone',
+        productNumber: '36378',
+        paymentStatus:'Refunded',
+        shipping:'Decline'
+    },
+    {
+        productName:'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Drone with Camera Drone',
+        productNumber: '96996',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'GPS 4K Drone',
+        productNumber: '22821',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Lozenge Drone',
+        productNumber: '00482',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
